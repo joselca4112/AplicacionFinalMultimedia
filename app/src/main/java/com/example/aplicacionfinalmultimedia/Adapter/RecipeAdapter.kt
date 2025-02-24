d793d5038e476a040f667e3e9939e54fc762a84f
joselu4112 <jcaragu537@g.educaand.es>
2025-02-24 14:45:55 +0100

    terminada

--- app/src/main/java/com/example/aplicacionfinalmultimedia/Adapter/RecipeAdapter.kt
+++ app/src/main/java/com/example/aplicacionfinalmultimedia/Adapter/RecipeAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -18,7 +19,7 @@ class RecipeAdapter(private var recipes: ArrayList<Recipe>, private val main: Ma
     inner class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val imageView: ImageView = itemView.findViewById(R.id.imageView)
-        val btnEliminar: Button =itemView.findViewById(R.id.btnEliminar)
+        val btnEliminar: ImageButton =itemView.findViewById(R.id.btnEliminar)
 
         init {
             itemView.setOnClickListener {
